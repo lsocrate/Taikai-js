157faa686943507920ff31288b68d28c07caa95d
Luiz Socrate <luiz@socrate.com.br>
2012-04-27 16:44:42 -0300

    Taikai.addPlayer

--- js/taikai.js
+++ js/taikai.js
@@ -1,5 +1,14 @@
+var Player = function (name) {
+  this.name = name;
+  this.clan = arguments[1] || 'unaligned';
+};
+
 var Taikai = function () {
   this.ranking = [];
   this.players = [];
   this.log = [];
+};
+
+Taikai.prototype.addPlayer = function(name, clan) {
+  this.players.push(new Player(name, clan));
 };
