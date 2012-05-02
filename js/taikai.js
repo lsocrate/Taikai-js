55346405c7d2100bf5e4d5c31a1a5e4a2d509525
Luiz Socrate <luiz@socrate.com.br>
2012-05-02 18:09:37 -0300

    taikai setMatch

--- js/taikai.js
+++ js/taikai.js
@@ -3,13 +3,19 @@ var Player = function (name) {
   this.clan = arguments[1] || 'unaligned';
 };
 
+var Match = function () {
+  this.players = [];
+};
+Match.prototype.addPlayer = function(name) {
+  this.players.push(name);
+};
+
 var Taikai = function () {
   this.ranking = [];
   this.players = {};
   this.rounds  = [];
   this.matches = [];
 };
-
 Taikai.prototype.addPlayer = function(name, clan) {
   this.players[name] = new Player(name, clan);
   this.ranking.push({
@@ -20,4 +26,15 @@ Taikai.prototype.addPlayer = function(name, clan) {
   });
 
   return this;
+};
+
+Taikai.prototype.setMatch = function() {
+  var players = arguments;
+  var match = new Match();
+
+  for (var i = 0; i < players.length; i++) {
+    match.addPlayer(players[i]);
+  };
+
+  this.matches.push(match);
 };
--- tests/tests/tests.js
+++ tests/tests/tests.js
@@ -1,6 +1,19 @@
+function getBaseTaikai() {
+  var taikai = new Taikai().addPlayer('Anna', 'Crab').addPlayer('Bob', 'Crab').addPlayer('Charles', 'Crab').addPlayer('David', 'Crab').addPlayer('Eduard', 'Crab');
+
+  return taikai;
+};
+
 test("Add player", function() {
   var taikai = new Taikai();
-  taikai.addPlayer('Siri', 'Scorpion');
-  equal(taikai.players.Siri.name, 'Siri');
-  equal(taikai.players.Siri.clan, 'Scorpion');
+  taikai.addPlayer('Anna', 'Scorpion');
+  equal(taikai.players.Anna.name, 'Anna');
+  equal(taikai.players.Anna.clan, 'Scorpion');
+});
+
+test("Set match", function() {
+  var taikai = getBaseTaikai();
+  taikai.setMatch('Anna', 'Bob', 'Charles', 'David');
+  console.log(taikai);
+  deepEqual(taikai.matches[0].players, ['Anna', 'Bob', 'Charles', 'David']);
 });
